0cb741359010051c7f3cda16ca1b0afbb37f25f7
Vkscoma <vkscoma@gmail.com>
2024-12-05 16:02:13 -0800

    Setting up input

--- showHidePassword/password-challenge/src/app/components/PasswordInput.tsx
+++ showHidePassword/password-challenge/src/app/components/PasswordInput.tsx
@@ -1,27 +1,30 @@
+"use client";
 import { Input } from "@/components/ui/input";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { useSpring, animated } from "@react-spring/web";
 
 export function InputWithButton() {
   return (
-    <div className="flex w-full max-w-sm items-center space-x-2">
+    <div className="flex w-full max-w-md items-center space-x-2">
       <div className="relative w-full">
         <Input
           type="password"
-          placeholder="Password"
-          className="pl-12 pr-12 bg-teal-400"
+          placeholder="enter password"
+          className="py-8 px-14 focus-visible:ring-0 bg-teal-500"
         />
         <Image
           src="/lock.svg"
           alt="Lock Icon"
-          width={20}
-          height={20}
-          className="absolute left-3 top-1/2 transform -translate-y-1/2 text-blue-900"
+          width={40}
+          height={40}
+          className="absolute left-1 top-1/2 transform -translate-y-1/2 border-r-2 border-white p-2"
         />
         <Image
           src="/eye.svg"
           alt="Eye Icon"
-          width={35}
-          height={35}
+          width={40}
+          height={40}
           className="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer bg-blue-900 rounded-full p-2"
         />
       </div>
